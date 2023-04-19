3f600113e598324dad10527d01a8f6bdeaeeba03
Taylor Nelson <taylornelson@Taylors-Laptop.fritz.box>
2023-04-19 14:21:54 +1200

    [story] Final touch up on side nav component

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -8,15 +8,27 @@ function SideNav() {
     <div className="side-nav-container">
       <div className="side-nav-links">
         <Link className="react-link" to="/create">
-          <IconButton iconType="plus" backgroundColor="rgb(56, 189, 255)" />
+          <IconButton
+            iconType="plus"
+            backgroundColor="rgb(56, 189, 255)"
+            diameter="45"
+          />
         </Link>
         <Link className="react-link" to="/saved">
-          <IconButton iconType="files" backgroundColor="rgb(56, 189, 255)" />
+          <IconButton
+            iconType="files"
+            backgroundColor="rgb(56, 189, 255)"
+            diameter="45"
+          />
         </Link>
       </div>
       <div className="side-nav-links">
         <Link className="react-link" to="/account">
-          <IconButton iconType="user" backgroundColor="rgb(56, 189, 255)" />
+          <IconButton
+            iconType="user"
+            backgroundColor="rgb(56, 189, 255)"
+            diameter="45"
+          />
         </Link>
       </div>
     </div>
