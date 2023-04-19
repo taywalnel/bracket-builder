1972a76437197962c6d5e4bf648ee967845da691
Taylor Nelson <taylornelson@Taylors-Laptop.fritz.box>
2023-04-19 14:07:45 +1200

    [story] Creates side nav component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CreateBracketPage from "./pages/CreateBracketPage/CreateBracketPage";
 import { useState } from "react";
 import ViewTournamentPage from "./pages/ViewBracketPage/ViewBracketPage";
 import HeaderBar from "./components/HeaderBar/HeaderBar";
+import SideNav from "./components/SideNav/SideNav";
 
 function App() {
   const [tournaments, setTournaments] = useState([]);
@@ -19,24 +20,29 @@ function App() {
   return (
     <div className="app-container">
       <HeaderBar />
-      <div className="app-route-outlet-wrapper">
-        <div className="app-route-outlet">
-          <Routes>
-            <Route
-              path="/create"
-              element={<CreateBracketPage saveNewBracket={saveNewTournament} />}
-            ></Route>
-            <Route path="/saved" element={<h1>saved</h1>}></Route>
-            <Route
-              path="/saved/:id"
-              element={
-                <ViewTournamentPage
-                  tournaments={tournaments}
-                  setTournaments={updateTournamentsHandler}
-                />
-              }
-            ></Route>
-          </Routes>
+      <div className="app-main-content-container">
+        <SideNav />
+        <div className="app-route-outlet-wrapper">
+          <div className="app-route-outlet">
+            <Routes>
+              <Route
+                path="/create"
+                element={
+                  <CreateBracketPage saveNewBracket={saveNewTournament} />
+                }
+              ></Route>
+              <Route path="/saved" element={<h1>saved</h1>}></Route>
+              <Route
+                path="/saved/:id"
+                element={
+                  <ViewTournamentPage
+                    tournaments={tournaments}
+                    setTournaments={updateTournamentsHandler}
+                  />
+                }
+              ></Route>
+            </Routes>
+          </div>
         </div>
       </div>
     </div>
